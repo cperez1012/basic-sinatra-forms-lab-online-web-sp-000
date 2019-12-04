dc679db32874e2907ab04ab0a057156a37a1a0ff
Christian Perez <perezchristian1012@gmail.com>
2019-12-04 11:31:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/team' do
+  post '/newteam' do
     @name = params['Team Name']
     @coach = params['Coach']
     @pg = params['Point Guard']
